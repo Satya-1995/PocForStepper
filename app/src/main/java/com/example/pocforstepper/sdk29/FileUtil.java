6dbbe0aec96a91646e0c2de6d67d6d23e0c06a08
satyajit.das <satyajit.das@cropin.com>
2021-11-03 16:13:51 +0530

    file copy

---
+++ app/src/main/java/com/example/pocforstepper/sdk29/FileUtil.java
@@ -0,0 +1,188 @@
+package com.example.pocforstepper.sdk29;
+
+import android.content.Context;
+import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
+import java.util.Date;
+
+public final class FileUtil {
+
+    private FileUtil() {
+        // This class is not publicly instantiable
+    }
+
+    interface FileCopyListener{
+        void fileCopyStatus(String fileName);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static File getOutputMediaFile() throws IOException {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory("Satyajit"),"testFolder");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_" + timeStamp + ".png");
+        return mediaFile;
+    }
+
+    public static String getOutputFolderName(Context context) {
+        File mediaStorageDir = new File(context.getFilesDir(), "UploadVideo");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        return mediaStorageDir.getPath();
+    }
+
+    public static void deleteFile(Context applicationContext, String uri) {
+        try {
+            File rawFile = new File(Uri.parse(uri).getPath());
+            boolean isDeletedRawFile = rawFile.delete();
+            if (!isDeletedRawFile) {
+                rawFile.getCanonicalFile().delete();
+            }
+            if (rawFile.exists()) {
+                applicationContext.deleteFile(rawFile.getName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void copyDirectoryOneLocationToAnotherLocation(File sourceLocation, File targetLocation,FileCopyListener mListener)
+            throws IOException {
+
+        if (sourceLocation.isDirectory()) {
+            if (!targetLocation.exists()) {
+                targetLocation.mkdir();
+            }
+
+            String[] children = sourceLocation.list();
+            for (int i = 0; i < sourceLocation.listFiles().length; i++) {
+                copyDirectoryOneLocationToAnotherLocation(new File(sourceLocation, children[i]),
+                        new File(targetLocation, children[i]),mListener);
+                mListener.fileCopyStatus(children[i].toLowerCase());
+            }
+        } else {
+
+            InputStream in = new FileInputStream(sourceLocation);
+
+            OutputStream out = new FileOutputStream(targetLocation);
+
+            // Copy the bits from instream to outstream
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        }
+
+    }
+
+//    public static void copy(File sourceLocation, File targetLocation) throws IOException {
+//
+//
+//        if (sourceLocation.isDirectory()) {
+//            if (!targetLocation.exists()) {
+//                targetLocation.mkdir();
+//            }
+//
+//            String[] children = sourceLocation.list();
+//            for (int i = 0; i < sourceLocation.listFiles().length; i++) {
+//                copy(new File(sourceLocation, children[i]),
+//                        new File(targetLocation, children[i]));
+////                mListener.fileCopyStatus(children[i].toLowerCase());
+//            }
+//        } else {
+//
+//
+//            FileInputStream inStream = new FileInputStream(sourceLocation);
+//            FileOutputStream outStream = new FileOutputStream(targetLocation);
+//            FileChannel inChannel = inStream.getChannel();
+//            FileChannel outChannel = outStream.getChannel();
+//            inChannel.transferTo(0, inChannel.size(), outChannel);
+//            inStream.close();
+//            outStream.close();
+//        }
+//    }
+
+    public static void copyFileOrDirectory(String srcDir, String dstDir,FileCopyListener mListener) {
+
+        try {
+            File src = new File(srcDir);
+            File dst = new File(dstDir, src.getName());
+
+            if (src.isDirectory()) {
+
+                String files[] = src.list();
+                int filesLength = files.length;
+                for (int i = 0; i < filesLength; i++) {
+                    File srcFile=new File(src, files[i]);
+                    String src1 = srcFile.getPath();
+                    String dst1 = dst.getPath();
+                    mListener.fileCopyStatus(srcFile.getName());
+                    copyFileOrDirectory(src1, dst1,mListener);
+                }
+            } else {
+                copyFile(src, dst);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.getParentFile().exists())
+            destFile.getParentFile().mkdirs();
+
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        } finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/pocforstepper/sdk29/ImageTest.java
@@ -0,0 +1,68 @@
+package com.example.pocforstepper.sdk29;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pocforstepper.R;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ImageTest extends AppCompatActivity {
+
+    private static final String TAG = "ImageTest";
+
+
+    private Button btnFetchImage;
+    private TextView tvSourcePath, tvDestinationPath, tvStatus;
+    File sourcePath, destinationPath = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_test);
+
+//        ivThumb = findViewById(R.id.iv_thumb);
+//        edImageName = findViewById(R.id.ed_image_name);
+        btnFetchImage = findViewById(R.id.btn_fetch_image);
+        tvSourcePath = findViewById(R.id.tv_source_path);
+        tvStatus = findViewById(R.id.tv_status);
+        tvDestinationPath = findViewById(R.id.tv_destination_path);
+
+        sourcePath = new File(Environment.getExternalStoragePublicDirectory("Cropin"), "farmer/");
+        destinationPath = ImageTest.this.getExternalFilesDir("");
+        tvSourcePath.setText(sourcePath.getAbsolutePath());
+        tvDestinationPath.setText(destinationPath.getAbsolutePath());
+        FileUtil.FileCopyListener mListener = fileName -> {
+            Log.d("TAG", "The name is " + fileName);
+            Toast.makeText(this,fileName,Toast.LENGTH_SHORT).show();
+        };
+
+
+        btnFetchImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Handler handler = new Handler();
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //FileUtil.copyDirectoryOneLocationToAnotherLocation(sourcePath, destinationPath, mListener);
+                        FileUtil.copyFileOrDirectory(sourcePath.getAbsolutePath(), destinationPath.getAbsolutePath(),mListener);
+                    }
+                });
+
+            }
+        });
+
+    }
+}
